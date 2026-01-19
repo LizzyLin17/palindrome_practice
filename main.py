42a9417c8be383bc4abfb1a7e37ce47302f4501a
LizzyLin <oolong.pumpking@gmail.com>
2026-01-19 21:05:49 +0800

    practice1

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def is_palindrome(text):
     clean = [c for c in nospace if c.isalnum()]
     cleaned = ''.join(clean)
 
-    upper1 = cleaned.upper()
+    upper = cleaned.upper()
 
-    return upper1 == upper1[::-1]
+    return upper == upper[::-1]
 
 
 if __name__ == '__main__':
